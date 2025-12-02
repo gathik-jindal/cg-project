d8ab272a014660b5271b1ee7bac778da2a98221f
gathii-jindal <gathik.jindal@iiitb.ac.in>
2025-12-02 23:52:29 +0530

    add deocrations

--- A3/src/index.js
+++ A3/src/index.js
@@ -44,7 +44,7 @@ async function main() {
     let meshBuffers;
     let lightCubeBuffers; // <--- Buffers for the light cube
     try {
-        const plyData = await loadPLY('../assets/beethoven.ply');
+        const plyData = await loadPLY('../assets/stratocaster.ply');
         meshBuffers = initBuffers(gl, plyData);
         lightCubeBuffers = initCubeBuffers(gl); // Initialize cube
     } catch (e) {
